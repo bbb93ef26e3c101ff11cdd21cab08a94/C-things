3a28e0ca413e4427f2a7a54149bad62306d52afd
EthernalVortex <77616414+EthernalVortex@users.noreply.github.com>
2023-03-30 10:03:42 +0300

    Add files via upload

---
+++ custom_memset.c
@@ -0,0 +1,14 @@
+VOID* 
+memSet(
+	IN VOID*  ptr, 
+	IN CHAR   Value, 
+	IN SIZE_T nSize
+)
+{
+	UCHAR* _ptr = ( UCHAR* )ptr; /* cast it (convert it from VOID to UCHAR) */
+	for ( INT i = 0; i < nSize; i++ ) /* iterate over size */
+	{
+		_ptr[ i ] = ( UCHAR )Value; /* sets _ptr to a value specified by user */
+	}
+	return _ptr; /* return ptr*/
+}
